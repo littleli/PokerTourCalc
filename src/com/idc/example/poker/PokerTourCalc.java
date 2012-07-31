3dc4398ce7c8e6cba90caaa81dd768b54abecb91
Ales Najmann <ales.najmann@gmail.com>
2012-08-01 01:31:05 +0200

    Initial commit

---
+++ src/com/idc/example/poker/PokerTourCalc.java
@@ -0,0 +1,13 @@
+package com.idc.example.poker;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class PokerTourCalc extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
